0e778f17fcd4bba4889e87c5874baa75b001565b
unknown <it.tiennguyen@gmail.com>
2024-09-30 14:18:46 +0700

    “SoSanhTuoi”

---
+++ so_sanh_tuoi/bin/so_sanh_tuoi.dart
@@ -0,0 +1,19 @@
+import 'package:so_sanh_tuoi/so_sanh_tuoi.dart' as so_sanh_tuoi;
+
+void main() 
+{
+  int agePerson1 = 50;
+  int agePerson2 = 90;
+  if(agePerson1 > agePerson2)
+  {
+    print('Tuổi người thứ nhất lớn hơn người thứ 2');
+  }
+  else if (agePerson1 < agePerson2)
+  {
+    print('Tuổi người thứ nhất nhỏ hơn người thứ 2');
+  }
+  else
+  {
+    print('Tuổi hai người bắng nhau');
+  } 
+}
---
+++ so_sanh_tuoi/test/so_sanh_tuoi_test.dart
@@ -0,0 +1,8 @@
+import 'package:so_sanh_tuoi/so_sanh_tuoi.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
