0b029d7a31ed34be2650c4cd91be53c7591b470e
unknown <it.tiennguyen@gmail.com>
2024-10-01 10:01:19 +0700

    “ChuyenDoiTienTe”

---
+++ chuyen_doi_tien_te/bin/chuyen_doi_tien_te.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main() {
+  int rate = 23000;
+
+  print ("Xin mời bạn nhập số lượng USD cần đổi:");
+  var Money = stdin.readLineSync();
+  var USD = int.tryParse(Money ?? "");
+  if (USD == null) {
+      print("bad year");
+  } else {
+      var VND = rate * USD;
+      print(VND);
+  }
+}
---
+++ chuyen_doi_tien_te/test/chuyen_doi_tien_te_test.dart
@@ -0,0 +1,8 @@
+import 'package:chuyen_doi_tien_te/chuyen_doi_tien_te.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
