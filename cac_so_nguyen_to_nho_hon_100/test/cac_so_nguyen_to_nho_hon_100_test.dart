0a38fc026cc209722a2712e8f03393c3318cbf39
unknown <it.tiennguyen@gmail.com>
2024-10-01 10:46:43 +0700

    “CacSoNguyenToNhoHon100”

---
+++ cac_so_nguyen_to_nho_hon_100/test/cac_so_nguyen_to_nho_hon_100_test.dart
@@ -0,0 +1,8 @@
+import 'package:cac_so_nguyen_to_nho_hon_100/cac_so_nguyen_to_nho_hon_100.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
