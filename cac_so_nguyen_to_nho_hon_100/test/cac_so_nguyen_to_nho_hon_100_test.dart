60a03ff54530696571728127673f7f327a121f9a
TienNguyen-47 <it.tiennguyen@gmail.com>
2024-10-01 11:53:54 +0700

    Delete cac_so_nguyen_to_nho_hon_100 directory

--- cac_so_nguyen_to_nho_hon_100/test/cac_so_nguyen_to_nho_hon_100_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:cac_so_nguyen_to_nho_hon_100/cac_so_nguyen_to_nho_hon_100.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
